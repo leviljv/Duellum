fb2340340d15512b46b09fa84cc5d925645e09fa
levi <levivermaning@gmail.com>
2023-12-19 16:11:42 +0100

    chore: add loop audio in eventmanager

--- Assets/Scripts/Managers/AudioHandler.cs
+++ Assets/Scripts/Managers/AudioHandler.cs
@@ -6,14 +6,24 @@ public class AudioHandler : MonoBehaviour
 
     private void OnEnable() {
         EventManager<AudioEvents, string>.Subscribe(AudioEvents.PlayAudio, audioManager.PlayAudio);
+        EventManager<AudioEvents, EventMessage<string, bool>>.Subscribe(AudioEvents.PlayLoopedAudio, PlayLoopedAudio);
     }
 
     private void OnDisable() {
         EventManager<AudioEvents, string>.Unsubscribe(AudioEvents.PlayAudio, audioManager.PlayAudio);
+        EventManager<AudioEvents, EventMessage<string, bool>>.Unsubscribe(AudioEvents.PlayLoopedAudio, PlayLoopedAudio);
     }
 
     private void Start() {
-        audioManager.PlayLoopedAudio("Music", true);
+        EventManager<AudioEvents, EventMessage<string, bool>>.Invoke(AudioEvents.PlayLoopedAudio, new("Music", true));
+    }
+
+    private void PlayLoopedAudio(EventMessage<string, bool> eventMessage) {
+
+        string audioClipName = eventMessage.value1;
+        bool isLooped = eventMessage.value2;
+
+        audioManager.PlayLoopedAudio(audioClipName, isLooped);
     }
 }
 
